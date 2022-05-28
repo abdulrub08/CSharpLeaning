cd1fa2f5f0cf83ce77fb65f5a88e7e7c0f249f68
abdulrub08 <abdulrub44469@gmail.com>
2022-05-28 11:00:13 +0530

    Adding Opps Session1

---
+++ CSharpOops/CSharpOops/Memory/MemoryDetails.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOopsDetails
+{
+    class MemoryDetails
+    {
+        public int age ;
+        public string name ;
+        public MemoryDetails()
+        {
+            age = 20;
+            name = "Test Data";
+        }
+        public void TestDate()
+        {
+
+        }
+        public MemoryDetails(int age,string name)
+        {
+            int x = 30;//Local Variable
+           
+            this.age = age ;
+            this.name = name;
+        }
+       
+
+    }
+}
---
+++ CSharpOops/CSharpOops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Zainul= CSharpOopsDetails;
+//using Ravish = CSharpOopsMethodDetails;
+using CSharpOopsDetails;
+
+namespace CSharpOops
+{
+    public class Program
+    {
+        
+        //Local Variable Stored i Stack Memory
+
+        //Instance variable Stored in Heap Memory
+
+        static void Main(string[] args)
+        {
+            MemoryDetails obj;
+            obj=new MemoryDetails();
+           // obj.age = 20;
+            MemoryDetails m1 = new MemoryDetails(25,"Tom");
+            //m1.age = 40;
+            //m1.name = "Zainul";
+            Console.WriteLine("User Name {0} age is {1}", obj.name, obj.age);
+            Console.WriteLine("User Name {0} age is {1}", m1.name, m1.age);
+            Console.ReadKey();
+        }
+    }
+}
